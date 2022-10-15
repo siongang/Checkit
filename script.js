50cae33b40778fb3d70b04a7b244f7c7cdb1e72f
siongang <98117983+siongang@users.noreply.github.com>
2022-10-15 15:43:42 -0400

    checkbox implementation

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
 
-let elem = '<div class= "checklist" id="last"><input type = "checkbox" name = "check" id = "check" class = "check" value = "finished"><input type = "text" class = "text"></div>';
+let elem = '<div class= "checklist" id="last"><input type = "checkbox" name = "check" id = "last-box" class = "check" value = "finished"><input type = "text" class = "text"></div>';
+
+let counter = 0;
+let checkArr = new Array(1).fill(false);
 
 
 let addMore = document.getElementById("add-button");
 console.log(addMore)
 
+
+
+
 if (addMore) {
     console.log('hi')
     addMore.addEventListener("click", ()=> {
+
         document.getElementById('last').id = 'temp';
         document.getElementById('temp').insertAdjacentHTML('afterend', elem);
-        document.getElementById('temp').id = null;
+        document.getElementById('temp').id = counter;
 
+
+        document.getElementById('last-box').id = counter;
+        counter++;
+        checkArr.push(false);
+        addListener();
         console.log("button pressed");
 
     });  
 }
+let checkBoxes = document.getElementsByClassName("check");
+addListener();
 
-/*
-let checkBox = document.getElementById("check")
+function addListener() {
+    
+    console.log(checkBoxes);
+    console.log(counter)
+    var box = checkBoxes.item(counter);
+    checkBoxes.item(counter).addEventListener("click", ()=>{  
+            console.log(box.id+"checkbox clicked");
 
-function strike(){
-    console.log('checkboxclicked')
-
+            /*
+            console.log(checkBoxes)
+            console.log('checked');
+            console.log("checkbox id is" + box.id)
+            checkArr[box.id] = !checkArr[box.id]
+            console.log(checkArr[box.id])
+            */
+        });
 }
-*/
